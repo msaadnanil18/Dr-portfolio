4f82e8b5d1302da6abd55c1b4507247a6080198e
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2023-12-28 11:56:52 +0000

    navbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,4 @@
-// import React from 'react';
-// import { Layout } from 'antd';
-
-// import About from './components/About';
-// import Services from './components/Services';
-// import Experience from './components/Experience';
-// import Contact from './components/Contect';
-// import Footer from './components/Footer';
-// import AppHeader from './components/AppHeader';
-// // import AppHeader from './components/Header';
-// // import Header from './components/Header';
-// // import About from './components/About';
-// // import Services from './components/Services';
-// // import Experience from './components/Experience';
-// // import Contact from './components/Contact';
-// // import Footer from './components/Footer';
-
-// const App = () => {
-//   return (
-//     <div>
-//       <AppHeader />
-//       <About />
-//       <Services />
-//       <Experience />
-//       <Contact />
-//       <Footer />
-//       <Layout/>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
 import About from './components/About';
 import Services from './components/Services';
@@ -42,26 +8,30 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <AppHeader />
-        {/* <About />
-        <Services />
-        <Experience />
-        <Contact />
-        <Footer /> */}
-      
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<h1>Welcome to Home!</h1>} />
-        </Routes>
-        <Footer />
+    <div>
+     <AppHeader />
+      <div id="home" className="section">
+     
       </div>
-    </Router>
+      <div id="about" className="section">
+      <About />
+      
+      </div>
+
+      <div id="Services" className="section">
+      <Services />
+      </div>
+
+      <div id="experience" className="section">
+      <Experience />
+      </div>
+      <div id="contact" className="section">
+      <Contact />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
 export default App;
+
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,60 +1,36 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
-import { animateScroll as scroll } from 'react-scroll';
-
-const { Header } = Layout;
+import { Menu } from 'antd';
+import { Link } from 'react-scroll';
 
 const AppHeader = () => {
-  const scrollToElement = (elementId) => {
-    // Scroll to the top of the page first
-    scroll.scrollToTop({
-      duration: 800,
-      delay: 0,
-      smooth: 'easeInOutQuart',
-    });
-
-    // Then scroll to the desired section
-    setTimeout(() => {
-      scroll.scrollTo(elementId, {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart',
-      });
-    }, 800); // Adjust the delay to match the duration of the scrollToTop animation
-  };
-
   return (
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">
-          <Link to="/" onClick={() => scrollToElement('homeSection')}>
-            Home
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link to="/about" onClick={() => scrollToElement('aboutSection')}>
-            About
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Link to="/services" onClick={() => scrollToElement('servicesSection')}>
-            Services
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Link to="/experience" onClick={() => scrollToElement('experienceSection')}>
-            Experience
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="5">
-          <Link to="/contact" onClick={() => scrollToElement('contactSection')}>
-            Contact
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </Header>
+    <Menu mode="horizontal" theme="dark">
+      <Menu.Item key="home">
+        <Link to="home" spy={true} smooth={true} duration={500}>
+          Home
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="about">
+        <Link to="about" spy={true} smooth={true} duration={500}>
+          About
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="Services">
+        <Link to="Services" spy={true} smooth={true} duration={500}>
+        Services
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="experience">
+        <Link to="experience" spy={true} smooth={true} duration={500}>
+          Experience
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="contact">
+        <Link to="contact" spy={true} smooth={true} duration={500}>
+          Contact
+        </Link>
+      </Menu.Item>
+    </Menu>
   );
 };
 
